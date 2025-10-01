03946a32c6406520d75eeec878e975e23c305bec
elena-debasabe <148590795+elena-debasabe@users.noreply.github.com>
2025-10-01 14:45:53 -0700

    cambio Elena

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          hola, sexo{count}
+          hola, caca{count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click ohola soy elena malvada jajajja odio los negros React logos to learn more
       </p>
     </>
   )
