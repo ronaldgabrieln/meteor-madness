ef96a9a2dadc36c14b8f4c37b92277ee521c7fae
Ronald Narain <ronaldgabrieln@gmail.com>
2025-10-04 08:27:37 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          hola, caca{count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click ohola soy elena malvada jajajja odio los negros React logos to learn more
-      </p>
+      
     </>
   )
 }
