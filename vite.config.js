bc18d84ff221750fffa64f8ed6034cc556a13606
Ronald Narain <ronaldgabrieln@gmail.com>
2025-10-07 10:44:46 -0700

    update in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'claude.html'),
+        simulation: resolve(__dirname, 'simulation.html'),
+        datasets: resolve(__dirname, 'useddatasets.html'),
+        defend: resolve(__dirname, 'defenderSlide/defendFirst.html')
+      }
+    }
+  }
 })
