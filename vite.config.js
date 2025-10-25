c23b950bd9cca4e48af4495aadd22b3c771f4f92
Ronald Narain <ronaldgabrieln@gmail.com>
2025-10-25 12:41:26 -0700

    solved build error

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'claude.html'),
+        main: resolve(__dirname, 'index.html'),
         simulation: resolve(__dirname, 'simulation.html'),
         datasets: resolve(__dirname, 'useddatasets.html'),
         defend: resolve(__dirname, 'defenderSlide/defendFirst.html')
